9c46cf66be6a7e3cc0b004263d870fbae530c935
Cuong Bien <biencaocuongg@gmail.com>
2026-07-07 12:21:46 +0700

    (feat): Vector magnitude computation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sensor',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: SensorExample(),
     );
   }
@@ -46,46 +45,47 @@ class _SensorExampleState extends State<SensorExample> {
         },
         onError: (e) {
           showDialog(
-              context: context,
-              builder: (context) {
-                return const AlertDialog(
-                  title: Text("Lỗi"),
-                  content: Text("Không tìm thấy cảm biến này trên thiết bị"),
-                );
-              });
+            context: context,
+            builder: (context) {
+              return const AlertDialog(
+                title: Text("Lỗi"),
+                content: Text("Không tìm thấy cảm biến này trên thiết bị"),
+              );
+            },
+          );
         },
         cancelOnError: true,
       ),
     );
     // 2. Lắng nghe User Accelerometer
     _streamSubscriptions.add(
-      userAccelerometerEventStream().listen(
-        (UserAccelerometerEvent event) {
-          setState(() {
-            _userAccelerometerValues = <double>[event.x, event.y, event.z];
-          });
-        },
-      ),
+      userAccelerometerEventStream().listen((UserAccelerometerEvent event) {
+        setState(() {
+          _userAccelerometerValues = <double>[event.x, event.y, event.z];
+        });
+      }),
     );
     // 3. Lắng nghe Gyroscope
     _streamSubscriptions.add(
-      gyroscopeEventStream().listen(
-        (GyroscopeEvent event) {
-          setState(() {
-            _gyroscopeValues = <double>[event.x, event.y, event.z];
-          });
-        },
-      ),
+      gyroscopeEventStream().listen((GyroscopeEvent event) {
+        setState(() {
+          _gyroscopeValues = <double>[event.x, event.y, event.z];
+        });
+      }),
     );
     // 4. Lắng nghe Magnetometer
     _streamSubscriptions.add(
-      magnetometerEventStream().listen(
-        (MagnetometerEvent event) {
-          setState(() {
-            _magnetometerValues = <double>[event.x, event.y, event.z];
-          });
-        },
-      ),
+      magnetometerEventStream().listen((MagnetometerEvent event) {
+        double x = event.x;
+        double y = event.y;
+        double z = event.z;
+
+        double magnitude = sqrt((x * x) + (y * y) + (z * z));
+        setState(() {
+          _magnetometerValues = <double>[x, y, z];
+          print("Độ lớn từ trường: ${magnitude.toStringAsFixed(2)} µT");
+        });
+      }),
     );
   }
 
@@ -100,10 +100,18 @@ class _SensorExampleState extends State<SensorExample> {
   @override
   Widget build(BuildContext context) {
     // Helper function để định dạng output
-    final accelerometer = _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final userAccelerometer = _userAccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final gyroscope = _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final magnetometer = _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final accelerometer = _accelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final gyroscope = _gyroscopeValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final magnetometer = _magnetometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
     return Scaffold(
       appBar: AppBar(title: const Text('Sensor Plus')),
       body: Center(
